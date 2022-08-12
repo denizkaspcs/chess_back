c94df5feea49290a70ae9541a43063046c6302de
denizkaspcs <denizkasapcs@gmail.com>
2022-08-12 16:56:09 +0300

    aa

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ io.on('connection', (socket) => {
     })
 })
 
-server.listen(8080, () => {
-    console.log('listening on *:8080');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to make quit.');
 });
